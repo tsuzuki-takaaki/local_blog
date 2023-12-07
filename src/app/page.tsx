a62d172dc82d54af1bb6f4e8668eadfc27a1e1e1
alexanderpierce <takaaki.110706@gmail.com>
2023-12-08 01:42:14 +0900

    Introduce prisma to Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
-export default function Home() {
+import { PrismaClient } from '@prisma/client'
+
+export default async function Home() {
+  const prisma = new PrismaClient()
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+
   return (
     <main>
+      { allUsers.map((user) => {
+        return <p>{user.name}</p>
+      })}
       <p>Hoge</p>
     </main>
   )
